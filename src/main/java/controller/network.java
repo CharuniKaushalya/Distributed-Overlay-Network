0a089a26d3b70786c5426a432b51e45d7a5cef17
KanchanaMalsha <malshathilini10.13@cse.mrt.ac.lk>
2017-10-23 23:59:39 +0530

    send UNREG

--- src/main/java/controller/network.java
+++ src/main/java/controller/network.java
@@ -6,6 +6,9 @@
 package main.java.controller;
 
 import main.java.model.config;
+import main.java.view.app;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
 
 import java.io.IOException;
 import java.net.DatagramPacket;
@@ -15,106 +18,116 @@ import java.util.*;
 import java.text.DecimalFormat;
 
 /**
- *
  * @author Charuni
  */
-public class network{
-	private DatagramSocket socket;
+public class network {
+
+    final static private Logger logger = Logger.getLogger(network.class);
+
+    private DatagramSocket socket;
     private int receivedMessages, sentMessages, unAnsweredMessages;
     private DecimalFormat formatter = new DecimalFormat("0000");
 
-	 public void run() throws IOException {
+    public network() {
+        BasicConfigurator.configure();
+    }
 
-	        boolean done = true;
-	        while (true) {
-	            if (done) {
-	                socket = new DatagramSocket(config.PORT);
-	                String msg = config.REG + " " + config.IP + " " + config.PORT + " " + config.USERNAME;
-	                sender(msg);
-	                done = false;
-	            }
-	            byte[] buffer = new byte[65536];
-	            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
-	            try {
-	                socket.receive(packet);
-	                byte[] data = packet.getData();
-	                String message = new String(data, 0, packet.getLength());
+    public void run() throws IOException {
 
-	                System.out.println("receiving ; " + message);
-	                receiver(message);
-	            //    onResponseReceived(message,new Node(packet.getAddress().getHostName(),packet.getPort()));
+        boolean done = true;
+        while (true) {
+            if (done) {
+                socket = new DatagramSocket(config.PORT);
+                String msg = config.REG + " " + config.IP + " " + config.PORT + " " + config.USERNAME;
+                sender(msg);
+                done = false;
+            }
+            byte[] buffer = new byte[65536];
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+            try {
+                socket.receive(packet);
+                byte[] data = packet.getData();
+                String message = new String(data, 0, packet.getLength());
 
-	            } catch (IOException e) {
-	                e.printStackTrace();
-	            }
+                logger.info("receiving ; " + message);
+                receiver(message);
+                //    onResponseReceived(message,new Node(packet.getAddress().getHostName(),packet.getPort()));
 
-	        }
-	    }
-	 
-	    private void sender(String msg) {
-            String length_final = formatter.format(msg.length() + 5);
-    		String msg_final = length_final + " " + msg;
-	        try {
-	            DatagramPacket packet = new DatagramPacket(msg_final.getBytes(), msg_final.getBytes().length,
-	            		 InetAddress.getByName(config.BOOTSTRAP_IP),config.BOOTSTRAP_PORT);
-	            socket.send(packet);
-	            sentMessages++;
-	        } catch (IOException e) {
-	            System.out.println(e);
-	        }
-	    }
-	    
-	    //will be invoked when a response is received
-	    private void receiver(String message) {
+            } catch (IOException e) {
+                logger.error(e);
+            }
 
-	        receivedMessages++;
-	        StringTokenizer tokenizer = new StringTokenizer(message, " ");
-	        String length = tokenizer.nextToken();
-	        String command = tokenizer.nextToken();
+        }
+    }
 
-	        if (config.REGOK.equals(command)) {
-	            int no_nodes = Integer.parseInt(tokenizer.nextToken());
+    public boolean leave() {
+        String msg = config.UNREG + " " + config.IP + " " + config.PORT + " " + config.USERNAME;
+        sender(msg);
+        return true;
+    }
 
-	            switch (no_nodes) {
-	                case 0:
-	                    break;
+    private void sender(String msg) {
+        String length_final = formatter.format(msg.length() + 5);
+        String msg_final = length_final + " " + msg;
+        try {
+            DatagramPacket packet = new DatagramPacket(msg_final.getBytes(), msg_final.getBytes().length,
+                    InetAddress.getByName(config.BOOTSTRAP_IP), config.BOOTSTRAP_PORT);
+            socket.send(packet);
+            sentMessages++;
+        } catch (IOException e) {
+            logger.error(e);
+        }
+    }
 
-	                case 1:
+    //will be invoked when a response is received
+    private void receiver(String message) {
 
-	                    System.out.println("Case 1");
-	                    break;
+        receivedMessages++;
+        StringTokenizer tokenizer = new StringTokenizer(message, " ");
+        String length = tokenizer.nextToken();
+        String command = tokenizer.nextToken();
 
-	                case 2:
-	                    System.out.println("Case 2");
-	                    break;
-	                case 9996:
-	                    System.out.println("Failed to register. BootstrapServer is full.");
-	                    break;
+        if (config.REGOK.equals(command)) {
+            int no_nodes = Integer.parseInt(tokenizer.nextToken());
 
-	                case 9997:
-	                    System.out.println("Failed to register. This ip and port is already used by another App.");
-	                    //closeSocket();
-	                    break;
+            switch (no_nodes) {
+                case 0:
+                    break;
 
-	                case 9998:
-	                    System.out.println("You are already registered. Please unregister first.");
-	                    break;
+                case 1:
 
-	                case 9999:
-	                    System.out.println("Error in the command. Please fix the error");
-	                   // closeSocket();
-	                    break;
-	            }
+                    logger.info("Case 1");
+                    break;
 
-	        } else if (config.UNROK.equals(command)) {
-	            System.out.println("Successfully unregistered this node from the boostrap server");
+                case 2:
+                    logger.info("Case 2");
+                    break;
+                case 9996:
+                    logger.info("Failed to register. BootstrapServer is full.");
+                    break;
 
-	        }else {
-	            unAnsweredMessages++;
-	        }
-	    }
-	
-    
+                case 9997:
+                    logger.info("Failed to register. This ip and port is already used by another App.");
+                    //closeSocket();
+                    break;
+
+                case 9998:
+                    logger.info("You are already registered. Please unregister first.");
+                    break;
+
+                case 9999:
+                    logger.info("Error in the command. Please fix the error");
+                    // closeSocket();
+                    break;
+            }
+
+        } else if (config.UNROK.equals(command)) {
+            logger.info("Successfully unregistered this node from the boostrap server");
+
+        } else {
+            unAnsweredMessages++;
+        }
+    }
 
 
 }
